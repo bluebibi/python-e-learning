1518ee64c77657f67c649fc9dc214fddb0dfa1c7
dohyung <tfedohk@gmail.com>
2017-11-18 22:04:57 +0900

    python code revised

---
+++ python3.6/Speech/Recognition/HMM.py
@@ -0,0 +1,11 @@
+def train():
+    print("Train")
+    pass
+
+def loadModel():
+    print("LoadModel")
+    pass
+
+def saveModel():
+    print("SaveModel")
+    pass
---
+++ python3.6/module_test.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def f():
+    print("Python is becoming popular.")
+
+    
+if __name__ == "__main__":
+    print(add(1, 10))
+    f()
